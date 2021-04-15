e1b5dda6e4037ec38467b5f7bdebcd278a9729b6
Nils Kopshoff <nils@kopshoff.koeln>
2021-04-15 11:43:39 +0200

    Preset values for adding formular
    To ease up the entry process of a new bloodpressure entity, date and time is now preset to the current date and time.

--- src/model/serializable/context.rs
+++ src/model/serializable/context.rs
@@ -1,3 +1,5 @@
+use chrono::Utc;
+use log::debug;
 use crate::model::serializable::bloodpressure::SerBloodPressure;
 use serde_derive::Serialize;
 
@@ -15,7 +17,27 @@ impl AppContext {
 }
 
 #[derive(Serialize)]
+pub struct AddBloodpressurePresetsContext {
+    pub current_date: String,
+    pub current_time: String
+}
+
+impl AddBloodpressurePresetsContext {
+    pub fn new() -> Self {
+        let cdatetime = Utc::now();
+        let cdate = cdatetime.date().naive_utc().to_string();
+        let ctime = cdatetime.time().format("%H:%M").to_string();
+        debug!("ctime: {}", ctime);
+        AddBloodpressurePresetsContext {
+            current_date: cdate,
+            current_time: ctime
+        }
+    }
+}
+
+#[derive(Serialize)]
 pub struct BloodpressureListContext {
     pub app_context: AppContext,
+    pub preset_context: AddBloodpressurePresetsContext,
     pub bloodpressures: Vec<SerBloodPressure>
 }
